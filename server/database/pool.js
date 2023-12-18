481c11e0091ad9cd42353530167ca95975f9cb56
Meby123B <meby123b@gmail.com>
2023-12-18 10:24:00 +0200

    update create tables

--- server/database/pool.js
+++ server/database/pool.js
@@ -9,75 +9,75 @@ const pool = new pg.Pool({
 
 async function createAllTables(){
     console.log(await pool.query(`
-    CREATE TABLE "players"(
-        "id" BIGINT NOT NULL,
-        "room_id" BIGINT NOT NULL,
-        "user_id" BIGINT NOT NULL
+    CREATE TABLE players(
+        id SERIAL NOT NULL PRIMARY KEY,
+        room_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL
     );
-    ALTER TABLE
-        "players" ADD PRIMARY KEY("id");
-    CREATE TABLE "rooms"(
-        "id" BIGINT NOT NULL,
-        "name" VARCHAR(255) NOT NULL,
-        "numOfPlayers" INTEGER NOT NULL,
-        "numOfRounds" INTEGER NOT NULL,
-        "manager" VARCHAR(255) NOT NULL
+
+    CREATE TABLE rooms(
+        id SERIAL NOT NULL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        numOfPlayers SMALLINT NOT NULL,
+        numOfRounds SMALLINT NOT NULL,
+        manager INTEGER NOT NULL
     );
-    ALTER TABLE
-        "rooms" ADD PRIMARY KEY("id");
-    CREATE TABLE "games"(
-        "id" BIGINT NOT NULL,
-        "name" VARCHAR(255) NOT NULL,
-        "description" VARCHAR(255) NOT NULL,
-        "settings" JSON NOT NULL,
-        "imageURL" VARCHAR(255) NOT NULL,
-        "genre" VARCHAR(255) NOT NULL
+
+
+    CREATE TABLE games(
+        id SERIAL NOT NULL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        description VARCHAR(255),
+        settings JSON,
+        imageURL VARCHAR(255),
+        genre VARCHAR(255)
     );
-    ALTER TABLE
-        "games" ADD PRIMARY KEY("id");
-    CREATE TABLE "rounds"(
-        "id" BIGINT NOT NULL,
-        "room_id" BIGINT NOT NULL,
-        "round_num" BIGINT NOT NULL,
-        "game_id" BIGINT NOT NULL,
-        "settings" JSON NOT NULL
+
+
+    CREATE TABLE rounds(
+        id SERIAL NOT NULL PRIMARY KEY,
+        room_id INTEGER NOT NULL,
+        round_num INTEGER NOT NULL,
+        game_id INTEGER NOT NULL,
+        settings JSON
     );
-    ALTER TABLE
-        "rounds" ADD PRIMARY KEY("id");
-    CREATE TABLE "users"(
-        "id" BIGINT NOT NULL,
-        "username" VARCHAR(255) NULL,
-        "password" VARCHAR(255) NULL,
-        "country" VARCHAR(255) NULL,
-        "email" VARCHAR(255) NULL,
-        "avatar" VARCHAR(255) NULL
+
+
+    CREATE TABLE users(
+        id SERIAL NOT NULL PRIMARY KEY,
+        username VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        country VARCHAR(255),
+        email VARCHAR(75) NOT NULL,
+        avatar VARCHAR(25) NOT NULL
     );
-    ALTER TABLE
-        "users" ADD PRIMARY KEY("id");
-    CREATE TABLE "results"(
-        "id" BIGINT NOT NULL,
-        "round_id" BIGINT NULL,
-        "user_id" BIGINT NULL,
-        "success" BOOLEAN NOT NULL,
-        "finish_time" TIME(0) WITHOUT TIME ZONE NOT NULL
+
+
+    CREATE TABLE results(
+        id SERIAL NOT NULL PRIMARY KEY,
+        round_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        success BOOLEAN NOT NULL,
+        finish_time INTEGER NOT NULL
     );
-    ALTER TABLE
-        "results" ADD PRIMARY KEY("id");
-    ALTER TABLE
-        "rounds" ADD CONSTRAINT "rounds_room_id_foreign" FOREIGN KEY("room_id") REFERENCES "rooms"("id");
-    ALTER TABLE
-        "rounds" ADD CONSTRAINT "rounds_game_id_foreign" FOREIGN KEY("game_id") REFERENCES "games"("id");
-    ALTER TABLE
-        "players" ADD CONSTRAINT "players_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
-    ALTER TABLE
-        "results" ADD CONSTRAINT "results_round_id_foreign" FOREIGN KEY("round_id") REFERENCES "rounds"("id");
-    ALTER TABLE
-        "players" ADD CONSTRAINT "players_room_id_foreign" FOREIGN KEY("room_id") REFERENCES "rooms"("id");
-    ALTER TABLE
-        "results" ADD CONSTRAINT "results_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
+
     
     `))
 
 }
+
+
+// ALTER TABLE
+// rounds ADD CONSTRAINTEGER rounds_room_id_foreign FOREIGN KEY(room_id) REFERENCES rooms(id);
+// ALTER TABLE
+// rounds ADD CONSTRAINTEGER rounds_game_id_foreign FOREIGN KEY(game_id) REFERENCES games(id);
+// ALTER TABLE
+// players ADD CONSTRAINTEGER players_user_id_foreign FOREIGN KEY(user_id) REFERENCES users(id);
+// ALTER TABLE
+// results ADD CONSTRAINTEGER results_round_id_foreign FOREIGN KEY(round_id) REFERENCES rounds(id);
+// ALTER TABLE
+// players ADD CONSTRAINTEGER players_room_id_foreign FOREIGN KEY(room_id) REFERENCES rooms(id);
+// ALTER TABLE
+// results ADD CONSTRAINTEGER results_user_id_foreign FOREIGN KEY(user_id) REFERENCES users(id);
 
 module.exports = pool
