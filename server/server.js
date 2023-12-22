02767c60fffafce157db90c3408f870a02cb652a
Meby123B <meby123b@gmail.com>
2023-12-22 12:19:59 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,21 +10,21 @@ const buildPath = pathMake.join(__dirname, 'build')
 const port = process.env.PORT || 8080
 
 app.use(cors())
-app.use('/assets',cors())
+app.use('/assets', cors())
 app.use(express.json())
 
 
-app.use('/assets',express.static(assetsPath))
+app.use('/assets', express.static(assetsPath))
 
- app.use('/',express.static(buildPath))
+app.use('/', express.static(buildPath))
 
 //app.get('/', (req, res) => {
-  //  res.send('hello world')
+//  res.send('hello world')
 //})
 
 const apiRouter = require('./routes/api')
-app.use('/api',apiRouter)
+app.use('/api', apiRouter)
 
 app.listen(port, () => {
-    console.log(`we are on http://localhost:${port}`);
+  console.log(`we are on http://localhost:${port}`);
 })
