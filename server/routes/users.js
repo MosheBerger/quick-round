783751eba3ac33cfe29f240985a9de7213ad4e4f
Meby123B <117580548+Meby123B@users.noreply.github.com>
2023-12-28 10:28:32 +0200

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,7 +10,7 @@ const router = express.Router()
 router.post('/login/:username', async (req, res, next) => {
     const client = req.client
     try {
-        const { username } = req.params
+        const { username} = req.params
         const { password } = req.body
         console.log('username', username);
         console.log('password', password);
@@ -67,6 +67,7 @@ router.post('/signup/:username', async (req, res, next) => {
     }
 })
 
+// user exist?
 router.get('/signup/:username', async (req, res, next) => {
     const client = req.client
     try {
@@ -75,10 +76,55 @@ router.get('/signup/:username', async (req, res, next) => {
         const isExist = await DB.checkIfExist(client, username)
         console.log(isExist);
 
-        if (!isExist)
+        if (isExist === undefined)
             throw { statusCode: 400, message: `an error append` }
 
         res.json({ result: isExist })
+        next()
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// SHOW PROFILE
+router.get('/show/:userId', async (req, res, next) => {
+    const client = req.client
+    try {
+        const { userId } = req.params
+
+        const user = await DB.showProfile(client, userId)
+        console.log(user);
+
+        if (user === undefined)
+            throw { statusCode: 400, message: `an error append` }
+
+        res.json({ user })
+        next()
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+// SHOW PROFILE
+router.put('/update/:username', async (req, res, next) => {
+    const client = req.client
+    try {
+        const { username } = req.params
+        const {password,avatar} = req.body
+
+        //todo
+        const user = await DB.updateAvatar(client, username, password, avatar)
+        console.log(user);
+
+        if (user === undefined)
+            throw { statusCode: 400, message: `an error append` }
+
+        res.json({ user })
         next()
 
     } catch (error) {
