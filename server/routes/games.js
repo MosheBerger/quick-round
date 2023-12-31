8f0e748033211f09e0e0daeb16d2725e86404c6c
Meby123B <meby123b@gmail.com>
2023-12-31 19:29:06 +0200

    finish apis !!! (?)

---
+++ server/routes/games.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const DB = require('../DB').games
+
+
+const router = express.Router()
+
+//GET
+// DB.show
+router.get('/:gameId', async (req, res, next) => {
+
+    const client = req.client
+    const { gameId } = req.params
+    try {
+        const game = await DB.showGame(client, gameId)
+
+        console.log(game);
+        res.json(game)
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// CREATE
+// DB.createGame
+router.post('/', async (req, res, next) => {
+
+    const client = req.client
+    const { name, description, settings, imageURL, genre } = req.body
+
+    try {
+        const newGame = await DB.createGame(client, name, description, settings, imageURL, genre)
+
+        console.log(newGame);
+        res.json(newGame)
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+//GET ALL
+// DB.showAll
+router.get('/', async (req, res, next) => {
+
+    const client = req.client
+    try {
+        const games = await DB.showAll(client)
+
+        console.log(games);
+        res.json(games)
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+module.exports = router
