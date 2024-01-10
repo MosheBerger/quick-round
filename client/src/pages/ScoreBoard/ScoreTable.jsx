6249962141a2f7bc27999a61ff87ad5553eb9430
Meby123B <meby123b@gmail.com>
2024-01-10 10:06:36 +0200

    Update ScoreTable.jsx

--- client/src/pages/ScoreBoard/ScoreTable.jsx
+++ client/src/pages/ScoreBoard/ScoreTable.jsx
@@ -7,32 +7,37 @@ function ScoreTable({ room, tableKey, title }) {
     return (<>
         <h3 className='unmargin'> {title} </h3>
         <article className='unmargin'>
-            <table>
-                <thead>
-                    <tr>
-                        <th> מספר סבב </th>
-                        <th> משחק </th>
-                        {room.players.map(p => <th>
-                            <div className='flex col'>
-                                <Avatar avatarSeed={p.avatar} />
-                                {p.username}
-                            </div>
-                        </th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {room[tableKey].map((row, i) => {
-                        const { round_num: num, game_id } = room.rounds[i]
-                        return <>
-                            <tr>
-                                <th>סבב {num}</th>
+            <figure>
+                <table >
+                    <thead>
+                        <tr>
+                            <th scope='col'> מספר סבב </th>
+                            <th scope='col'> משחק </th>
+
+                            {room.players.map(p => (
+                                <th key={p.id} scope='col'>
+                                    <div className='flex col'>
+                                        <Avatar avatarSeed={p.avatar} />
+                                        {p.username}
+                                    </div>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {room[tableKey].map((row, i) => {
+
+                            const { round_num: num, game_id, id } = room.rounds[i]
+
+                            return <tr key={id}>
+                                <th scope='row'>{num}</th>
                                 <th>{<GameCard id={game_id} style={{ maxWidth: 150 }} />}</th>
-                                {row.map((result) => <td> <TimingCard result={result}/> </td>)}
+                                {row.map((result, i) => <td key={i}> <TimingCard result={result} /> </td>)}
                             </tr>
-                        </>
-                    })}
-                </tbody>
-            </table>
+                        })}
+                    </tbody>
+                </table>
+            </figure>
         </article>
 
     </>)
