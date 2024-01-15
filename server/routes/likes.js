177836cdd288f5ba1a88e83f2644bf1b7f48c872
Meby123B <meby123b@gmail.com>
2024-01-15 17:47:36 +0200

    Update likes.js

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -5,21 +5,6 @@ const likesDB = require('../DB').likes
 const router = express.Router()
 
 
-router.get('/:userId', async (req, res, next) => {
-    const client = req.client
-    const { userId } = req.params
-
-    try {
-        const results = await likesDB.showLikedByUser(client, userId)
-
-        console.log(results);
-        res.json(results)
-
-        next()
-    } catch (error) {
-        next(error)
-    }
-})
 
 // CREATE 
 router.post('/', async (req, res, next) => {
@@ -54,32 +39,6 @@ router.put('/', async (req, res, next) => {
         next(error)
     }
 })
-
-
-// REMOVE?
-// DB.remove
-
-// GET ONE
-// DB.show
-
-// GET ALL IN SPECIFIC ROOM
-router.get('/in-room/:roomId', async (req, res, next) => {
-    const client = req.client
-    const { roomId } = req.params
-
-    try {
-        const rounds = await roundsDB.showByRoom(client, roomId)
-
-        console.log(rounds);
-        res.json(rounds)
-
-        next()
-    } catch (error) {
-        next(error)
-    }
-})
-
-
 
 
 module.exports = router
