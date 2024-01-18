19448a6780950c015239ba54edf229e938c68ce2
Meby123B <meby123b@gmail.com>
2024-01-18 10:28:06 +0200

    Update pool.js

--- server/sql/pool.js
+++ server/sql/pool.js
@@ -4,7 +4,7 @@ const pg = require('pg')
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.SSL
+    ssl: process.env.SSLO
 })
 
 module.exports = pool
