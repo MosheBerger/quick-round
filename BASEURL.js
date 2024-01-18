5754921ba398787b9da370e5fb9c65ee5bd0e6de
Meby123B <meby123b@gmail.com>
2024-01-18 09:12:25 +0200

    fix path on deploiment

--- BASEURL.js
+++ BASEURL.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://localhost:8080'
-const BASE_URL = 'https://quick-round.onrender.com/'
+const BASE_URL = 'https://quick-round.onrender.com'
 // const BASE_URL = 'http://192.168.199.216:8080'
 // const BASE_URL = 'http://192.168.1.180:8080'
 export default BASE_URL
